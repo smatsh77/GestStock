3bfad0e51a7b103a9cedbf87f8edc1424519bda7
sam_hach <samir.hachemi@umontreal.com>
2019-01-07 19:25:23 -0500

    Début projet

---
+++ GestStock/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Prism.Unity;
+
+
+namespace GestStock
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.TryResolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = (Window)Shell;
+            Application.Current.MainWindow?.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+        }
+    }
+}
